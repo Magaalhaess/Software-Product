225695f165513253f90c7ed7d4a721631faa1f8e
Magaalhaess <112771029+Magaalhaess@users.noreply.github.com>
2023-09-17 23:13:08 -0300

    Add files via upload

---
+++ controle.py
@@ -0,0 +1,25 @@
+from PyQt5 import uic,QtWidgets
+
+def funcao_principal():
+    linha1 = formulario.lineEdit.text()
+    linha2 = formulario.lineEdit_2.text()
+    linha3 = formulario.lineEdit_3.text()
+
+    if formulario.radioButton.isChecked():
+        print("Categoria Eletrônicos foi selecionada")
+    elif formulario.radioButton_2.isChecked():
+        print("Categoria Informatica foi selecionada") 
+    else:
+        print("Categoria Alimentos foi selecionada")
+
+    print("código:", linha1)
+    print("descrição:", linha2)
+    print("preço:", linha3)
+
+
+app=QtWidgets.QApplication([])
+formulario=uic.loadUi("formulario2.ui")
+formulario.pushButton.clicked.connect(funcao_principal)
+
+formulario.show()
+app.exec()
