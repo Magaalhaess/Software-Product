7035ed41ec9eeffe55c3d06f141edd45fd04ece9
Magaalhaess <112771029+Magaalhaess@users.noreply.github.com>
2023-10-15 17:07:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 from PyQt5 import uic,QtWidgets
+import mysql.connector
+
+banco = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "030298",
+    database = "cadastro_produtos"
+)
 
 def funcao_principal():
     linha1 = formulario.lineEdit.text()
     linha2 = formulario.lineEdit_2.text()
     linha3 = formulario.lineEdit_3.text()
 
+    categoria = ""
+
     if formulario.radioButton.isChecked():
         print("Categoria Eletrônicos foi selecionada")
+        categoria = "Eletrônicos"
     elif formulario.radioButton_2.isChecked():
         print("Categoria Informatica foi selecionada") 
+        categoria = "Informatica"
     else:
         print("Categoria Alimentos foi selecionada")
+        categoria = "Alimentos"
 
-    print("código:", linha1)
-    print("descrição:", linha2)
-    print("preço:", linha3)
+    print("ID:", linha1)
+    print("NOME:", linha2)
+    print("VALOR:", linha3)
+    cursor = banco.cursor()
+    comando_SQL = "INSERT INTO produtos (codigo, nome, valor, categoria) VALUES (%s, %s, %s, %s)"
+    dados = (str(linha1),str(linha2),str(linha3),categoria)
+    cursor.execute(comando_SQL,dados)
+    banco.commit()
 
 
 app=QtWidgets.QApplication([])
