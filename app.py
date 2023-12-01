39dcf31fdcc631f6b57170dc51bde87efc39848e
Magaalhaess <112771029+Magaalhaess@users.noreply.github.com>
2023-12-01 15:42:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from PyQt5 import uic,QtWidgets
 import mysql.connector
+from reportlab.pdfgen import canvas
+
+numero_id = 0
 
 banco = mysql.connector.connect(
     host = "localhost",
@@ -7,6 +10,78 @@ banco = mysql.connector.connect(
     passwd = "030298",
     database = "cadastro_produtos"
 )
+
+def editar_dados():
+    global numero_id
+    linha = segunda_tela.tableWidget.currentRow()
+   
+    cursor = banco.cursor()
+    cursor.execute("SELECT id FROM produtos")
+    dados_lidos = cursor.fetchall()
+    valor_id = dados_lidos[linha][0]
+    cursor.execute("SELECT * FROM produtos WHERE id="+ str(valor_id))
+    produto = cursor.fetchall()
+    tela_editar.show()
+
+    numero_id = valor_id
+
+    tela_editar.lineEdit.setText(str(produto[0][0]))
+    tela_editar.lineEdit_2.setText(str(produto[0][1]))
+    tela_editar.lineEdit_3.setText(str(produto[0][2]))
+    tela_editar.lineEdit_4.setText(str(produto[0][3]))
+    tela_editar.lineEdit_5.setText(str(produto[0][4]))
+
+def salvar_dados_editados():
+    global numero_id
+    codigo = tela_editar.lineEdit_2.text()
+    nome = tela_editar.lineEdit_3.text()
+    valor = tela_editar.lineEdit_4.text()
+    categoria = tela_editar.lineEdit_5.text()
+
+    cursor=banco.cursor()
+    cursor.execute("UPDATE produtos SET codigo = '{}', nome = '{}', valor = '{}', categoria = '{}' WHERE id = {}". format(codigo,nome,valor,categoria,numero_id))
+
+    tela_editar.close()
+    segunda_tela.close()
+    chama_segunda_tela()
+
+def excluir_dados():
+    linha = segunda_tela.tableWidget.currentRow()
+    segunda_tela.tableWidget.removeRow(linha)
+
+    cursor = banco.cursor()
+    cursor.execute("SELECT id FROM produtos")
+    dados_lidos = cursor.fetchall()
+    valor_id = dados_lidos[linha][0]
+    cursor.execute("DELETE FROM produtos WHERE id ="+ str(valor_id))
+
+def gerar_pdf():
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+    y = 0
+    pdf = canvas.Canvas("cadastro_produtos.pdf")
+    pdf.setFont("Times-Bold", 25)
+    pdf.drawString(200,800, "Produtos cadastrados:")
+    pdf.setFont("Times-Bold", 18)
+
+    pdf.drawString(10,750, "ID")
+    pdf.drawString(110,750, "CODIGO")
+    pdf.drawString(210,750, "NOME")
+    pdf.drawString(310,750, "VALOR")
+    pdf.drawString(410,750, "CATEGORIA")
+
+    for i in range(0, len(dados_lidos)):
+        y = y + 50
+        pdf.drawString(10,750 - y, str(dados_lidos[i][0]))
+        pdf.drawString(110,750 - y, str(dados_lidos[i][1]))
+        pdf.drawString(210,750 - y, str(dados_lidos[i][2]))
+        pdf.drawString(310,750 - y, str(dados_lidos[i][3]))
+        pdf.drawString(410,750 - y, str(dados_lidos[i][4]))
+
+    pdf.save()
+    print("PDF FOI GERADO COM SUCESSO!")
 
 def funcao_principal():
     linha1 = formulario.lineEdit.text()
@@ -41,12 +116,29 @@ def funcao_principal():
 def chama_segunda_tela():
     segunda_tela.show()
 
+    cursor = banco.cursor()
+    comando_SQL = "SELECT * FROM produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+
+    segunda_tela.tableWidget.setRowCount(len(dados_lidos))
+    segunda_tela.tableWidget.setColumnCount(5)
+
+    for i in range(0, len(dados_lidos)):
+        for j in range(0,5):
+            segunda_tela.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+
 
 app=QtWidgets.QApplication([])
 formulario=uic.loadUi("cadastro.ui")
 segunda_tela=uic.loadUi("listar_dados.ui")
+tela_editar=uic.loadUi("menu_editar.ui")
 formulario.pushButton.clicked.connect(funcao_principal)
 formulario.pushButton_2.clicked.connect(chama_segunda_tela)
+segunda_tela.pushButton.clicked.connect(gerar_pdf)
+segunda_tela.pushButton_2.clicked.connect(excluir_dados)
+segunda_tela.pushButton_3.clicked.connect(editar_dados)
+tela_editar.pushButton.clicked.connect(salvar_dados_editados)
 
 formulario.show()
 app.exec()
